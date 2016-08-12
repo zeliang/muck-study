bb26d31c9f0cb3889825a596841c1dc9459c447f
muck <muck@fcuh.com>
2016-08-12 15:45:17 +0800

    .

--- muck-study-spark/src/main/java/com/muck/study/spark/SparkDataFrame.java
+++ muck-study-spark/src/main/java/com/muck/study/spark/SparkDataFrame.java
@@ -1,13 +1,23 @@
 package com.muck.study.spark;
 
+import java.io.Serializable;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.api.java.JavaSQLContext;
 import org.apache.spark.sql.api.java.JavaSchemaRDD;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class SparkDataFrame extends BaseMain {
 
-	public static class People {
+	public static class People implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private Integer age;
 		private String name;
 
@@ -46,11 +56,17 @@ public class SparkDataFrame extends BaseMain {
 
 		JavaSchemaRDD teenagers = sqlContext
 				.sql("select * from people where age >=13 and age <= 19");
-
-		teenagers.map(row -> {
+		JavaRDD<People> peopleRdd = teenagers.map(row -> {
 			People one = new People();
 			one.setAge(row.getInt(0));
+			one.setName(row.getString(1));
 			return one;
 		});
+
+		List<People> list = peopleRdd.collect();
+		list.stream().forEach(people -> {
+			System.out.println(JSONObject.toJSONString(people));
+		});
+
 	}
 }
